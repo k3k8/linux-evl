126e2e9cf5cee265449e9101985c78d660e13847
Philippe Gerum <rpm@xenomai.org>
2016-07-20 15:06:15 +0200

    genirq: irq_pipeline: add IRQ pipeline core
    Signed-off-by: Philippe Gerum <rpm@xenomai.org>
    
    genirq: irq_pipeline: fix irq flow handling with stacked irqchips
    
    When irqchips are stacked, a cascaded event decoded by a parent irq
    which is not delivered oob won't receive any ack/eoi, which is
    wrong. Failing to ack any unmasked edge event could quickly yield an
    interrupt storm. E.g. on x86, with a gpio irq controller stacked on
    top of the IO/APIC:
    
    handle_irq_pipelined_prepare()
    [on_pipeline_entry: true]
    arch_handle_irq()
            handle_fasteoi_irq()
                    handle_oob_irq()
                            mask_cond_eoi_irq()
    handle_irq_pipelined_finish()
    [on_pipeline_entry: false]
            synchronize_pipeline_on_irq()
                    handle_fasteoi_irq() [!on_pipeline_entry]
                          intel_gpio_irq()
                               handle_edge_irq() <<< failed to ack
    
    To solve this, we need to track the deferral status of every irq
    event, running the latter through the pipeline injection code when
    required, regardless of the current stage.
    
    See https://lore.kernel.org/xenomai/MN2PR11MB42855A234CF8BF7D17E9E2ECF2E6A@MN2PR11MB4285.namprd11.prod.outlook.com/.
    
    Signed-off-by: Philippe Gerum <rpm@xenomai.org>
    Signed-off-by: Hongzhan Chen <hongzhan.chen@intel.com>

--- init/main.c
+++ init/main.c
@@ -55,6 +55,7 @@
 #include <linux/tick.h>
 #include <linux/sched/isolation.h>
 #include <linux/interrupt.h>
+#include <linux/irq_pipeline.h>
 #include <linux/taskstats_kern.h>
 #include <linux/delayacct.h>
 #include <linux/unistd.h>
@@ -917,6 +918,7 @@ void start_kernel(void)
 	char *command_line;
 	char *after_dashes;
 
+	stall_inband_nocheck();
 	set_task_stack_end_magic(&init_task);
 	smp_setup_processor_id();
 	debug_objects_early_init();
@@ -924,7 +926,7 @@ void start_kernel(void)
 
 	cgroup_init_early();
 
-	local_irq_disable();
+	local_irq_disable_full();
 	early_boot_irqs_disabled = true;
 
 	/*
@@ -972,6 +974,7 @@ void start_kernel(void)
 	setup_log_buf(0);
 	vfs_caches_init_early();
 	sort_main_extable();
+	irq_pipeline_init_early();
 	trap_init();
 	mm_core_init();
 	poking_init();
@@ -1018,6 +1021,7 @@ void start_kernel(void)
 	/* init some links before init_ISA_irqs() */
 	early_irq_init();
 	init_IRQ();
+	irq_pipeline_init();
 	tick_init();
 	rcu_init_nohz();
 	init_timers();
@@ -1040,7 +1044,7 @@ void start_kernel(void)
 	WARN(!irqs_disabled(), "Interrupts were enabled early\n");
 
 	early_boot_irqs_disabled = false;
-	local_irq_enable();
+	local_irq_enable_full();
 
 	kmem_cache_init_late();
 
