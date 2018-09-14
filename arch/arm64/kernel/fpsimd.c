95bb5b0a337c67ee11aef7f55e9795fd74bfbe4b
Philippe Gerum <rpm@xenomai.org>
2018-09-14 11:17:29 +0200

    arm64: dovetail: route traps
    Signed-off-by: Philippe Gerum <rpm@xenomai.org>
    
    arm64: dovetail: Fix undefinstr/break trap handling
    
    When running an compat RT application on arm64 the break trap is
    handled via the undefined instruction trap.
    
    A possible call stack looks like this:
    
    Call trace:
      handle_inband_event+0x2d0/0x320
      inband_event_notify+0x28/0x50
      signal_wake_up_state+0x7c/0xa4
      complete_signal+0x104/0x2d0
      __send_signal_locked+0x1d0/0x3e4
      send_signal_locked+0xf0/0x140
      force_sig_info_to_task+0xa0/0x164
      force_sig_fault+0x64/0x94
      arm64_force_sig_fault+0x48/0x80
      send_user_sigtrap+0x50/0x8c
      aarch32_break_handler+0xac/0x1d0
      do_undefinstr+0x6c/0x360
      el0_undef+0x4c/0xd0
      el0t_32_sync_handler+0xd0/0x140
      el0t_32_sync+0x190/0x194
    
    The trap is never reported to the companion core at that stage so
    running_oob() in do_undefinstr() will always return true. As the
    following bailout happens before calling the compat breakpoint
    detection (aarch32_break_handler()) debugging the compat
    application does not work.
    
    In addition the emulation of the deprecated armv8 SWP{B} instruction
    (try_emulate_armv8_deprecated()) cannot be done from the out-of-band
    stage.
    
    Therefore do_undefinstr()/do_el0_undef reports the trap entry to the
    companion core. If the companion core handles the undefined instruction,
    running_oob returns true and the bailout occurs. Otherwise, switching to
    the in-band stage takes place and the undefined instruction handler
    continues with the compat breakpoint detection and the SWP{B}
    instruction emulation.
    
    Signed-off-by: Clara Kowalsky <clara.kowalsky@siemens.com>

--- arch/arm64/kernel/fpsimd.c
+++ arch/arm64/kernel/fpsimd.c
@@ -1387,16 +1387,18 @@ static void sve_init_regs(void)
  */
 void do_sve_acc(unsigned long esr, struct pt_regs *regs)
 {
+	oob_trap_notify(ARM64_TRAP_SVE, regs);
+
 	/* Even if we chose not to use SVE, the hardware could still trap: */
 	if (unlikely(!system_supports_sve()) || WARN_ON(is_compat_task())) {
 		force_signal_inject(SIGILL, ILL_ILLOPC, regs->pc, 0);
-		return;
+		goto out;
 	}
 
 	sve_alloc(current, true);
 	if (!current->thread.sve_state) {
 		force_sig(SIGKILL);
-		return;
+		goto out;
 	}
 
 	get_cpu_fpsimd_context();
@@ -1414,6 +1416,8 @@ void do_sve_acc(unsigned long esr, struct pt_regs *regs)
 	sve_init_regs();
 
 	put_cpu_fpsimd_context();
+out:
+	oob_trap_unwind(ARM64_TRAP_SVE, regs);
 }
 
 /*
@@ -1429,10 +1433,14 @@ void do_sve_acc(unsigned long esr, struct pt_regs *regs)
  */
 void do_sme_acc(unsigned long esr, struct pt_regs *regs)
 {
+	unsigned long flags;
+
+	oob_trap_notify(ARM64_TRAP_SME, regs);
+
 	/* Even if we chose not to use SME, the hardware could still trap: */
 	if (unlikely(!system_supports_sme()) || WARN_ON(is_compat_task())) {
 		force_signal_inject(SIGILL, ILL_ILLOPC, regs->pc, 0);
-		return;
+		goto out;
 	}
 
 	/*
@@ -1441,14 +1449,14 @@ void do_sme_acc(unsigned long esr, struct pt_regs *regs)
 	 */
 	if (ESR_ELx_SME_ISS_SMTC(esr) != ESR_ELx_SME_ISS_SMTC_SME_DISABLED) {
 		force_signal_inject(SIGILL, ILL_ILLOPC, regs->pc, 0);
-		return;
+		goto out;
 	}
 
 	sve_alloc(current, false);
 	sme_alloc(current, true);
 	if (!current->thread.sve_state || !current->thread.sme_state) {
 		force_sig(SIGKILL);
-		return;
+		goto out;
 	}
 
 	get_cpu_fpsimd_context();
@@ -1468,6 +1476,8 @@ void do_sme_acc(unsigned long esr, struct pt_regs *regs)
 	}
 
 	put_cpu_fpsimd_context();
+out:
+	oob_trap_unwind(ARM64_TRAP_SME, regs);
 }
 
 /*
@@ -1508,9 +1518,13 @@ void do_fpsimd_exc(unsigned long esr, struct pt_regs *regs)
 			si_code = FPE_FLTRES;
 	}
 
+	oob_trap_notify(ARM64_TRAP_FPE, regs);
+
 	send_sig_fault(SIGFPE, si_code,
 		       (void __user *)instruction_pointer(regs),
 		       current);
+
+	oob_trap_unwind(ARM64_TRAP_FPE, regs);
 }
 
 static void fpsimd_load_kernel_state(struct task_struct *task)
