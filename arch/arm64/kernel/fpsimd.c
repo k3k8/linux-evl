3745f4b16e2cf2a7247c547c3afa954557aef6e0
Philippe Gerum <rpm@xenomai.org>
2018-09-14 11:17:29 +0200

    arm64: dovetail: route traps
    Signed-off-by: Philippe Gerum <rpm@xenomai.org>

--- arch/arm64/kernel/fpsimd.c
+++ arch/arm64/kernel/fpsimd.c
@@ -1400,16 +1400,18 @@ static void sve_init_regs(void)
  */
 void do_sve_acc(unsigned long esr, struct pt_regs *regs)
 {
+	oob_trap_notify(ARM64_TRAP_SVE, regs);
+
 	/* Even if we chose not to use SVE, the hardware could still trap: */
 	if (unlikely(!system_supports_sve()) || WARN_ON(is_compat_task())) {
 		force_signal_inject(SIGILL, ILL_ILLOPC, regs->pc, 0);
-		return;
+		goto out;
 	}
 
 	sve_alloc(current, true);
 	if (!current->thread.sve_state) {
 		force_sig(SIGKILL);
-		return;
+		goto out;
 	}
 
 	get_cpu_fpsimd_context();
@@ -1427,6 +1429,8 @@ void do_sve_acc(unsigned long esr, struct pt_regs *regs)
 	sve_init_regs();
 
 	put_cpu_fpsimd_context();
+out:
+	oob_trap_unwind(ARM64_TRAP_SVE, regs);
 }
 
 /*
@@ -1442,10 +1446,14 @@ void do_sve_acc(unsigned long esr, struct pt_regs *regs)
  */
 void do_sme_acc(unsigned long esr, struct pt_regs *regs)
 {
+	unsigned long flags;
+
+	oob_trap_notify(ARM64_TRAP_SME, regs);
+
 	/* Even if we chose not to use SME, the hardware could still trap: */
 	if (unlikely(!system_supports_sme()) || WARN_ON(is_compat_task())) {
 		force_signal_inject(SIGILL, ILL_ILLOPC, regs->pc, 0);
-		return;
+		goto out;
 	}
 
 	/*
@@ -1454,14 +1462,14 @@ void do_sme_acc(unsigned long esr, struct pt_regs *regs)
 	 */
 	if (ESR_ELx_ISS(esr) != ESR_ELx_SME_ISS_SME_DISABLED) {
 		force_signal_inject(SIGILL, ILL_ILLOPC, regs->pc, 0);
-		return;
+		goto out;
 	}
 
 	sve_alloc(current, false);
 	sme_alloc(current, true);
 	if (!current->thread.sve_state || !current->thread.za_state) {
 		force_sig(SIGKILL);
-		return;
+		goto out;
 	}
 
 	get_cpu_fpsimd_context();
@@ -1479,6 +1487,8 @@ void do_sme_acc(unsigned long esr, struct pt_regs *regs)
 	}
 
 	put_cpu_fpsimd_context();
+out:
+	oob_trap_unwind(ARM64_TRAP_SME, regs);
 }
 
 /*
@@ -1510,9 +1520,13 @@ void do_fpsimd_exc(unsigned long esr, struct pt_regs *regs)
 			si_code = FPE_FLTRES;
 	}
 
+	oob_trap_notify(ARM64_TRAP_FPE, regs);
+
 	send_sig_fault(SIGFPE, si_code,
 		       (void __user *)instruction_pointer(regs),
 		       current);
+
+	oob_trap_unwind(ARM64_TRAP_FPE, regs);
 }
 
 void fpsimd_thread_switch(struct task_struct *next)
