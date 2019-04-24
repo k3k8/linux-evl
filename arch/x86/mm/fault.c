57ac21c71bddae488f674b4eb8975d0394f135eb
Philippe Gerum <rpm@xenomai.org>
2019-04-24 16:40:19 +0200

    x86: irq_pipeline: add IRQ pipeline core
    Signed-off-by: Philippe Gerum <rpm@xenomai.org>
    Signed-off-by: Jan Kiszka <jan.kiszka@siemens.com>
    
    x86: dovetail: Fix Intel shadow stack support
    
    Let fpregs_lock_and_load return the saved flags of fpregs_lock and make
    sure that the shadow stack callers use that properly.
    
    Signed-off-by: Jan Kiszka <jan.kiszka@siemens.com>

--- arch/x86/mm/fault.c
+++ arch/x86/mm/fault.c
@@ -797,7 +797,7 @@ __bad_area_nosemaphore(struct pt_regs *regs, unsigned long error_code,
 	 * User mode accesses just cause a SIGSEGV.
 	 * It's possible to have interrupts off here:
 	 */
-	local_irq_enable();
+	local_irq_enable_full();
 
 	/*
 	 * Valid to do another page fault here because this one came
@@ -1482,7 +1482,7 @@ handle_page_fault(struct pt_regs *regs, unsigned long error_code,
 	 * page fault handling might have reenabled interrupts,
 	 * make sure to disable them again.
 	 */
-	local_irq_disable();
+	local_irq_disable_full();
 }
 
 DEFINE_IDTENTRY_RAW_ERRORCODE(exc_page_fault)
