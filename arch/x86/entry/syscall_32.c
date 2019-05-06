44e28824beb5c37e6a1ce0ff10836680c8f02ae6
Philippe Gerum <rpm@xenomai.org>
2019-05-06 18:37:41 +0200

    x86: dovetail: route syscalls
    Signed-off-by: Philippe Gerum <rpm@xenomai.org>
    
    dovetail: x86: Fix detection of prctl based compat syscalls
    
    In case a compat application is requesting a dovetail service the
    syscall number will not be equal to __NR_prctl (= the x86_64 syscall
    number).
    
    The same problem exists on the arm64 architecture where the problem was
    already addressed. Apply the same to x86.
    
    Signed-off-by: Florian Bezdeka <florian.bezdeka@siemens.com>
    
    dovetail: arch: Fix fetching of the first syscall argument from compat tasks
    
    On x86 there is a small difference in the ABI (= register) where the
    first syscall argument is supplied between the x86_64 (native) and
    compat (i386) case.
    
    On x86_64 the DI register is being used, while i386 used the BX
    register.
    
    The same problem exists for further arguments - handled by
    syscall_get_arguments() - as well. syscall_get_arg0() is dovetail
    specific and is now in sync with syscall_get_arguments().
    
    Signed-off-by: Florian Bezdeka <florian.bezdeka@siemens.com>
    
    dovetail: x86: Fix detection of prctl based compat syscalls
    
    In case a compat application is requesting a dovetail service the
    syscall number will not be equal to __NR_prctl (= the x86_64 syscall
    number).
    
    The same problem exists on the arm64 architecture where the problem was
    already addressed. Apply the same to x86.
    
    Signed-off-by: Florian Bezdeka <florian.bezdeka@siemens.com>
    
    dovetail: arch: Fix fetching of the first syscall argument from compat tasks
    
    On x86 there is a small difference in the ABI (= register) where the
    first syscall argument is supplied between the x86_64 (native) and
    compat (i386) case.
    
    On x86_64 the DI register is being used, while i386 used the BX
    register.
    
    The same problem exists for further arguments - handled by
    syscall_get_arguments() - as well. syscall_get_arg0() is dovetail
    specific and is now in sync with syscall_get_arguments().
    
    Signed-off-by: Florian Bezdeka <florian.bezdeka@siemens.com>
    
    x86: dovetail: Fix compat syscall handling, account for new IDT entry
    
    The following upstream commit introduces a new IDTENTRY for handling
    compat syscall. As that commit is now backported into stable trees it
    broke compat syscall handling for dovetail.
    
    be5341eb0d43 ("x86/entry: Convert INT 0x80 emulation to IDTENTRY")
    
    We no longer end up in do_int80_syscall_32() which has the dovetail
    specific bits. We need the same handling in asm_int80_emulation().
    
    The problematic part in asm_int80_emulation() is the call to
    do_syscall_32_irqs_on() which basically overwrites regs->ax
    unconditionally. Linux does not know about the co-kernel syscalls, so
    we get -ENOSYS for each syscall result.
    
    Signed-off-by: Florian Bezdeka <florian.bezdeka@siemens.com>

--- arch/x86/entry/syscall_32.c
+++ arch/x86/entry/syscall_32.c
@@ -167,10 +167,22 @@ __visible noinstr void do_int80_emulation(struct pt_regs *regs)
 
 	nr = syscall_32_enter(regs);
 
-	local_irq_enable();
+	syscall_enter_from_user_enable_irqs();
 	nr = syscall_enter_from_user_mode_work(regs, nr);
+
+	if (dovetailing()) {
+		if (nr == EXIT_SYSCALL_OOB) {
+			hard_local_irq_disable();
+			instrumentation_end();
+			return;
+		}
+		if (nr == EXIT_SYSCALL_TAIL)
+			goto done;
+	}
+
 	do_syscall_32_irqs_on(regs, nr);
 
+done:
 	instrumentation_end();
 	syscall_exit_to_user_mode(regs);
 }
@@ -253,12 +265,23 @@ __visible noinstr void do_int80_syscall_32(struct pt_regs *regs)
 	 * the semantics of syscall_get_nr().
 	 */
 	nr = syscall_enter_from_user_mode(regs, nr);
+
+	if (dovetailing()) {
+		if (nr == EXIT_SYSCALL_OOB) {
+			hard_local_irq_disable();
+			return;
+		}
+		if (nr == EXIT_SYSCALL_TAIL)
+			goto done;
+	}
+
 	instrumentation_begin();
 
 	add_random_kstack_offset();
 	do_syscall_32_irqs_on(regs, nr);
 
 	instrumentation_end();
+done:
 	syscall_exit_to_user_mode(regs);
 }
 #endif /* !CONFIG_IA32_EMULATION */
@@ -277,7 +300,7 @@ static noinstr bool __do_fast_syscall_32(struct pt_regs *regs)
 
 	instrumentation_begin();
 	add_random_kstack_offset();
-	local_irq_enable();
+	syscall_enter_from_user_enable_irqs();
 	/* Fetch EBP from where the vDSO stashed it. */
 	if (IS_ENABLED(CONFIG_X86_64)) {
 		/*
@@ -303,9 +326,20 @@ static noinstr bool __do_fast_syscall_32(struct pt_regs *regs)
 
 	nr = syscall_enter_from_user_mode_work(regs, nr);
 
+	if (dovetailing()) {
+		if (nr == EXIT_SYSCALL_OOB) {
+			instrumentation_end();
+			hard_local_irq_disable();
+			return true;
+		}
+		if (nr == EXIT_SYSCALL_TAIL)
+			goto done;
+	}
+
 	/* Now this is just like a normal syscall. */
 	do_syscall_32_irqs_on(regs, nr);
 
+done:
 	instrumentation_end();
 	syscall_exit_to_user_mode(regs);
 	return true;
--- include/linux/entry-common.h
+++ include/linux/entry-common.h
@@ -37,6 +37,14 @@
 				 SYSCALL_WORK_SYSCALL_USER_DISPATCH |	\
 				 SYSCALL_WORK_SYSCALL_EXIT_TRAP)
 
+/*
+ * Status codes of syscall entry when Dovetail is enabled. Must not
+ * conflict with valid syscall numbers. And with -1 which seccomp uses
+ * to skip an syscall.
+ */
+#define EXIT_SYSCALL_OOB	(-2)
+#define EXIT_SYSCALL_TAIL	(-3)
+
 /**
  * arch_ptrace_report_syscall_entry - Architecture specific ptrace_report_syscall_entry() wrapper
  * @regs: Pointer to the register state at syscall entry
@@ -143,6 +151,17 @@ static __always_inline long syscall_trace_enter(struct pt_regs *regs, unsigned l
 static __always_inline long syscall_enter_from_user_mode_work(struct pt_regs *regs, long syscall)
 {
 	unsigned long work = READ_ONCE(current_thread_info()->syscall_work);
+	int ret;
+
+	/*
+	 * Pipeline the syscall to the companion core if the current
+	 * task wants this. Compiled out if not dovetailing.
+	 */
+	ret = pipeline_syscall(syscall, regs);
+	if (ret > 0)		/* out-of-band, bail out. */
+		return EXIT_SYSCALL_OOB;
+	if (ret < 0)		/* in-band, tail work only. */
+		return EXIT_SYSCALL_TAIL;
 
 	if (work & SYSCALL_WORK_ENTER)
 		syscall = syscall_trace_enter(regs, work);
