10e57b928461a0073dbe87f3715cccd55c4454ec
Philippe Gerum <rpm@xenomai.org>
2020-04-07 10:43:35 +0200

    KVM: x86: dovetail: enable coexistence with out-of-band scheduling
    Make the KVM/x86 virtualization support coexist with out-of-band
    scheduling, so that a Dovetail-enabled kernel can safely run virtual
    machines and autonomous cores concurrently.
    
    The implementation is closely derived from the I-pipe tree commit
    gcac2d4cd5 maintained at https://gitlab.denx.de/Xenomai/ipipe-x86.
    
    Signed-off-by: Philippe Gerum <rpm@xenomai.org>
    
    KVM: x86: dovetail: Move handling async page faults to the inband stage
    
    Dovetail was stuck in the boot phase as KVM guest on a system that
    reported the asynchronous page fault handling feature.
    
    The apic_eoi() call in sysvec_kvm_asyncpf_interrupt() has no effect in
    case CONFIG_IRQPIPELINE is enabled, thus those IRQs / vectors where
    never ACKed at APIC level, the CPUs stalled.
    
    Instead of migrating to __apic_oie() the IDT entry for
    HYPERVISOR_CALLBACK_VECTOR for KVM on x86 is now marked as PIPELINED.
    Other architectures (and other guest extensions) already used
    DECLARE_IDTENTRY_SYSVEC_PIPELINED().
    
    The imbalance of how HYPERVISOR_CALLBACK_VECTOR is being used /
    pipelined goes back to 5.10. Backporting might be required.
    
    Signed-off-by: Florian Bezdeka <florian.bezdeka@siemens.com>
    
    KVM: x86: dovetail: Allow kvm_wait() to be called from any context
    
    kvm_wait() unconditionally enabled HW interrupts when called from
    pipeline entry code. That broke the PV (paravirtualized) spinlock
    implementation.
    
    The following calling contexts are supported now:
      - pipeline entry, hard IRQs off:
        Wakeup on vCPU kicks only, triggered by the current lock owner via
        hypercall. Using halt().
    
      - Linux IRQ context, hard IRQs on, in-band IRQs off:
        Wakeup on the next hard IRQ, or by a vCPU kick.
        Using native_safe_halt() to statisfy lockdep.
    
      - Task context, hard IRQs on, in-band IRQs on:
        Same as Linux IRQ context.
    
    The !CONFIG_IRQ_PIPELINE part is now re-synced with the version of
    Linux to keep trace events.
    
    Signed-off-by: Florian Bezdeka <florian.bezdeka@siemens.com>
    
    x86: dovetail: Allow multiple HV guest extensions to be enabled
    
    do_sysvec_inband() assumed that only one of CONFIG_ACRN_GUEST and
    CONFIG_KVM_GUEST is active at the same time. If both were enabled we
    run into a compile issue:
    
    arch/x86/kernel/irq_pipeline.c: In function 'do_sysvec_inband':
    arch/x86/kernel/irq_pipeline.c:160:9: error: duplicate case value
       160 |         case HYPERVISOR_CALLBACK_VECTOR:
           |         ^~~~
    arch/x86/kernel/irq_pipeline.c:154:9: note: previously used here
       154 |         case HYPERVISOR_CALLBACK_VECTOR:
           |         ^~~~
    
    As only one hypervisor (HV) can be active at the same time, but support
    for more than one can be compiled in, we have to migrate from a build
    time decision to a runtime based one.
    
    All HYPERVISOR_CALLBACK_VECTOR related callsites of alloc_intr_gate()
    have been migrated to sysvec_install(). sysvec_install() is able to
    distinguish between the low level entry point (asm_ variant) and the
    higher entry point (__ variant).
    
    The trampoline function (pipeline_hv_callback()) is needed to statisfy
    type checks done by run_sysvec_on_irqstack_cond().
    
    Reported-by: Ionut Nechita <ionut.nechita@intel.com>
    Closes: https://lore.kernel.org/xenomai/PH0PR11MB59498BE915597AEF0B5BBB30F4B42@PH0PR11MB5949.namprd11.prod.outlook.com/
    Signed-off-by: Florian Bezdeka <florian.bezdeka@siemens.com>

--- arch/x86/include/asm/idtentry.h
+++ arch/x86/include/asm/idtentry.h
@@ -238,6 +238,13 @@ __visible __noreturn void __##func(struct pt_regs *regs)
 #define DEFINE_IDTENTRY_SYSVEC_SIMPLE_PIPELINED(vector, func)		\
 	DEFINE_IDTENTRY_SYSVEC_PIPELINED(vector, func)
 
+extern void (*pipeline_hv_callback_fn)(struct pt_regs *regs);
+static inline void pipeline_install_sysvec(int vector, const void *function)
+{
+	if (vector == HYPERVISOR_CALLBACK_VECTOR)
+		pipeline_hv_callback_fn = function;
+}
+
 #else  /* !CONFIG_IRQ_PIPELINE */
 
 #define DECLARE_IDTENTRY_SYSVEC_PIPELINED(vector, func)			DECLARE_IDTENTRY_SYSVEC(vector, func)
@@ -277,6 +284,9 @@ __visible noinstr void func(struct pt_regs *regs,			\
 }									\
 									\
 static noinline void __##func(struct pt_regs *regs, u32 vector)
+
+#define pipeline_install_sysvec(__vector, __function)	\
+	do { (void)__vector; } while (0)
 
 #endif	/* !CONFIG_IRQ_PIPELINE */
 
@@ -527,6 +537,7 @@ static inline void fred_install_sysvec(unsigned int vector, const idtentry_t fun
 #endif
 
 #define sysvec_install(vector, function) {				\
+	pipeline_install_sysvec(vector, __##function);			\
 	if (cpu_feature_enabled(X86_FEATURE_FRED))			\
 		fred_install_sysvec(vector, function);			\
 	else								\
@@ -842,7 +853,7 @@ DECLARE_IDTENTRY_SYSVEC_PIPELINED(HYPERVISOR_CALLBACK_VECTOR,	sysvec_xen_hvm_cal
 #endif
 
 #ifdef CONFIG_KVM_GUEST
-DECLARE_IDTENTRY_SYSVEC(HYPERVISOR_CALLBACK_VECTOR,	sysvec_kvm_asyncpf_interrupt);
+DECLARE_IDTENTRY_SYSVEC_PIPELINED(HYPERVISOR_CALLBACK_VECTOR,	sysvec_kvm_asyncpf_interrupt);
 #endif
 
 #undef X86_TRAP_OTHER
