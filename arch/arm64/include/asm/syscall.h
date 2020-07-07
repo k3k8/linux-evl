16653491f23a03beff527d55e3c586c5fffa0311
Philippe Gerum <rpm@xenomai.org>
2020-07-07 18:39:36 +0200

    arm64: dovetail: route syscalls
    Signed-off-by: Philippe Gerum <rpm@xenomai.org>
    
    dovetail: arm64: Fix detection of prctl based compat syscalls
    
    In case a compat application is requesting a dovetail service the
    syscall number will not be equal to __NR_prctl (= the x86_64 syscall
    number).
    
    The same problem exists on the arm64 architecture where the problem was
    already addressed. Apply the same to x86.
    
    Signed-off-by: Florian Bezdeka <florian.bezdeka@siemens.com>
    
    dovetail: arch: Fix fetching of the first syscall argument from compat tasks
    
    On x86 there is a small difference in the ABI (= register) where the
    first syscall argument is supplied between the x86_64 (native) and
    compat (i386) case.
    
    On x86_64 the DI register is being used, while i386 used the BX
    register.
    
    The same problem exists for further arguments - handled by
    syscall_get_arguments() - as well. syscall_get_arg0() is dovetail
    specific and is now in sync with syscall_get_arguments().
    
    Signed-off-by: Florian Bezdeka <florian.bezdeka@siemens.com>

--- arch/arm64/include/asm/syscall.h
+++ arch/arm64/include/asm/syscall.h
@@ -79,6 +79,12 @@ static inline void syscall_set_nr(struct task_struct *task,
 
 #define SYSCALL_MAX_ARGS 6
 
+static inline unsigned long syscall_get_arg0(struct task_struct *task,
+					     struct pt_regs *regs)
+{
+	return regs->orig_x0;
+}
+
 static inline void syscall_get_arguments(struct task_struct *task,
 					 struct pt_regs *regs,
 					 unsigned long *args)
