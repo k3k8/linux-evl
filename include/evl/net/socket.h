1a34fadc34d5a30df62995a24552ec9ad35e4e80
Philippe Gerum <philippe.gerum@exail.com>
2025-03-25 15:08:07 +0100

    evl/net: provide neighbour solicitation service
    We need a way to guarantee zero-delay output to a given IP
    destination, which implies to resolve all routing and neighbour
    solicitation chores beforehand, so that we won't have to pass on the
    initial output to the inband stack, waiting for the oob front caches
    (IP routes and ARP) to be updated on the fly.
    
    To this end, ioctl(EVL_SOCKIOC_SOLICIT) is introduced for any IPv4 EVL
    socket (UDP so far). This request waits for the EVL front caches to
    receive the information needed to complete an output strictly from the
    oob stage, triggering a route resolution and neighbour solicitation as
    required.
    
    Signed-off-by: Philippe Gerum <philippe.gerum@exail.com>

--- include/evl/net/socket.h
+++ include/evl/net/socket.h
@@ -39,6 +39,8 @@ struct evl_net_proto {
 	int (*shutdown)(struct evl_socket *esk, int how);
 	int (*ioctl)(struct evl_socket *esk, unsigned int cmd,
 		unsigned long arg);
+	int (*solicit)(struct evl_socket *esk,
+		struct sockaddr *addr, int flags);
 	ssize_t (*oob_send)(struct evl_socket *esk,
 			const struct user_oob_msghdr __user *u_msghdr,
 			struct iovec *iov,
