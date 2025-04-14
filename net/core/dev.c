e10c1433e0b13733c78fc97151bffb63d1b6604c
Philippe Gerum <philippe.gerum@exail.com>
2025-04-14 11:43:28 +0200

    net: dovetail: fold napi_complete_oob() into napi_schedule_oob()
    So far, napi_schedule_oob() was invoked from the oob stage
    exclusively, aimed at queuing a NAPI struct for RX handling by the
    companion core from the oob stage.
    
    On the other hand, napi_complete_oob() was expected to schedule the RX
    handling of the packets accepted (in-band) by netif_deliver_oob() from
    a non oob-capable device diverting traffic to the companion core, so
    that it passes those packets to the proper protocol handlers in the
    oob netstack.
    
    We can ask the companion core to fold the logic of napi_complete_oob()
    into napi_schedule_oob() by checking the current execution stage,
    requiring a single hook for both cases.
    
    Signed-off-by: Philippe Gerum <philippe.gerum@exail.com>

--- net/core/dev.c
+++ net/core/dev.c
@@ -5384,9 +5384,6 @@ static void process_inband_rx_backlog(struct softnet_data *sd)
 __weak void napi_schedule_oob(struct napi_struct *n)
 { }
 
-__weak void napi_complete_oob(struct napi_struct *n)
-{ }
-
 __weak void process_inband_tx_backlog(struct softnet_data *sd)
 { }
 
@@ -5406,9 +5403,6 @@ static inline bool netif_receive_oob_list(struct list_head *head)
 }
 
 static inline void napi_schedule_oob(struct napi_struct *n)
-{ }
-
-static inline void napi_complete_oob(struct napi_struct *n)
 { }
 
 static inline void process_inband_tx_backlog(struct softnet_data *sd)
@@ -6529,7 +6523,7 @@ bool napi_complete_done(struct napi_struct *n, int work_done)
 	if (netif_oob_diversion(n->dev)) {
 		if (net_running_oob())
 			return true;
-		napi_complete_oob(n);
+		napi_schedule_oob(n);
 	}
 
 	/*
