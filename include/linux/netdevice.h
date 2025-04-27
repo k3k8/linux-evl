c8ac5b7702db8d3ddf361f8d9d702a75c53ca27b
Philippe Gerum <philippe.gerum@exail.com>
2025-04-27 15:43:24 +0200

    net: dovetail: add stage exclusion to tx_lock
    Signed-off-by: Philippe Gerum <philippe.gerum@exail.com>

--- include/linux/netdevice.h
+++ include/linux/netdevice.h
@@ -4497,6 +4497,7 @@ static inline void __netif_tx_lock(struct netdev_queue *txq, int cpu)
 	spin_lock(&txq->_xmit_lock);
 	/* Pairs with READ_ONCE() in __dev_queue_xmit() */
 	WRITE_ONCE(txq->xmit_lock_owner, cpu);
+	netif_tx_lock_oob(txq);
 }
 
 static inline bool __netif_tx_acquire(struct netdev_queue *txq)
@@ -4515,6 +4516,7 @@ static inline void __netif_tx_lock_bh(struct netdev_queue *txq)
 	spin_lock_bh(&txq->_xmit_lock);
 	/* Pairs with READ_ONCE() in __dev_queue_xmit() */
 	WRITE_ONCE(txq->xmit_lock_owner, smp_processor_id());
+	netif_tx_lock_oob(txq);
 }
 
 static inline bool __netif_tx_trylock(struct netdev_queue *txq)
@@ -4524,12 +4526,14 @@ static inline bool __netif_tx_trylock(struct netdev_queue *txq)
 	if (likely(ok)) {
 		/* Pairs with READ_ONCE() in __dev_queue_xmit() */
 		WRITE_ONCE(txq->xmit_lock_owner, smp_processor_id());
+		netif_tx_lock_oob(txq);
 	}
 	return ok;
 }
 
 static inline void __netif_tx_unlock(struct netdev_queue *txq)
 {
+	netif_tx_unlock_oob(txq);
 	/* Pairs with READ_ONCE() in __dev_queue_xmit() */
 	WRITE_ONCE(txq->xmit_lock_owner, -1);
 	spin_unlock(&txq->_xmit_lock);
@@ -4537,6 +4541,7 @@ static inline void __netif_tx_unlock(struct netdev_queue *txq)
 
 static inline void __netif_tx_unlock_bh(struct netdev_queue *txq)
 {
+	netif_tx_unlock_oob(txq);
 	/* Pairs with READ_ONCE() in __dev_queue_xmit() */
 	WRITE_ONCE(txq->xmit_lock_owner, -1);
 	spin_unlock_bh(&txq->_xmit_lock);
