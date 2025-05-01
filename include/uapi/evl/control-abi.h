344571a69a21f1bbbce0aab47a38a6fd8c2eb865
Philippe Gerum <philippe.gerum@exail.com>
2025-05-01 12:26:10 +0200

    evl/net: socket: introduce set/get opt requests
    With the upcoming support for packet timestamping, we are going to
    have more socket options, time has come to provide for dedicated
    set/get option requests instead of handling those directly from the
    common (inband) ioctl handler.
    
    EVL_SOCIOC_{SET, GET}OPT ioctl codes now redirect to generic
    per-socket option handlers. Currently, all options are set/retrieved
    from the inband stage, assuming this is merely prep work before oob
    I/O transactions take place.
    
    As a result, SENDSZ and RECVSZ ioctl requests have been converted to
    socket options.
    
    Signed-off-by: Philippe Gerum <philippe.gerum@exail.com>

--- include/uapi/evl/control-abi.h
+++ include/uapi/evl/control-abi.h
@@ -10,14 +10,14 @@
 #include <linux/types.h>
 
 /* Earliest ABI level we support. */
-#define EVL_ABI_BASE   36
+#define EVL_ABI_BASE   39
 /*
  * Current/latest ABI level we support. We may decouple the base and
  * current ABI levels by providing backward compatibility from the
  * latter to the former. CAUTION: a litteral value is required for the
  * current ABI definition (scripts reading this may be naive).
  */
-#define EVL_ABI_LEVEL  38
+#define EVL_ABI_LEVEL  39
 
 #define EVL_CONTROL_DEV  "/dev/evl/control"
 
