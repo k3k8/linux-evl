331bbb951af0aedcd2295dfea76aa93c0cb81de5
Philippe Gerum <philippe.gerum@exail.com>
2025-05-11 17:04:41 +0200

    net: dovetail: forward set/getsockopt() calls to the oob core
    Signed-off-by: Philippe Gerum <philippe.gerum@exail.com>

--- include/net/sock.h
+++ include/net/sock.h
@@ -1955,6 +1955,14 @@ int sock_oob_bind(struct sock *sk,
 
 int sock_oob_shutdown(struct sock *sk, int how);
 
+int sock_inband_setopt_redirect(struct sock *sk,
+				int level, int optname,
+				sockptr_t optval, unsigned int optlen);
+
+int sock_inband_getopt_redirect(struct sock *sk,
+				int level, int optname,
+				sockptr_t optval, int *optlen);
+
 static inline void sock_oob_destruct(struct sock *sk)
 {
 	void sock_oob_destroy(struct sock *sk);
@@ -1962,7 +1970,7 @@ static inline void sock_oob_destruct(struct sock *sk)
 		sock_oob_destroy(sk);
 }
 
-#else
+#else  /* !CONFIG_NET_OOB */
 
 static inline bool sock_oob_capable(struct socket *sock)
 {
@@ -1980,9 +1988,23 @@ static inline int sock_oob_shutdown(struct sock *sk, int how)
 	return 0;
 }
 
+static inline int sock_inband_setopt_redirect(struct sock *sk,
+					int level, int optname,
+					sockptr_t optval, int optlen)
+{
+	return 0;
+}
+
+static inline int sock_inband_getopt_redirect(struct sock *sk,
+					int level, int optname,
+					sockptr_t optval, int *optlen)
+{
+	return 0;
+}
+
 static inline void sock_oob_destruct(struct sock *sk) { }
 
-#endif
+#endif	/* !CONFIG_NET_OOB */
 
 int sk_setsockopt(struct sock *sk, int level, int optname,
 		  sockptr_t optval, unsigned int optlen);
