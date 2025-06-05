489a93c48f4168dcdcee071bf26951d9ae65b79f
Philippe Gerum <philippe.gerum@exail.com>
2025-06-05 17:31:49 +0200

    evl/net: enable oob traffic via loopback devices
    This patch allows for enabling 'lo' as an oob networking port. When
    enabled, the loopback device accepts back any oob traffic which it
    receives.
    
    As a consequence, packets sent to 127.0.0.1 from the oob stage will be
    delivered to the 'lo' oob port. Conversely, in-band traffic sent to
    the loopback interface will be delivered to the in-band
    stack. Loopback devices won't generally support VLANs, however we
    still allow incoming in-band traffic to be directed to the oob port by
    mean of a BPF program.
    
    Signed-off-by: Philippe Gerum <philippe.gerum@exail.com>

--- include/evl/net/route.h
+++ include/evl/net/route.h
@@ -35,6 +35,10 @@ static inline void evl_net_put_route(struct evl_net_route *ert)
 	evl_put_cache_entry(&ert->entry);
 }
 
+void evl_net_prepare_routing(struct net_device *dev);
+
+void evl_net_unprepare_routing(struct net_device *dev);
+
 void evl_net_flush_routes(struct net *net, struct net_device *dev);
 
 static inline struct dst_entry *evl_net_route_dst(const struct evl_net_route *ert)
