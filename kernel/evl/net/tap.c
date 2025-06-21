92779f056f695dbe1d5cf5620a7dc55cf2e000e7
Philippe Gerum <philippe.gerum@exail.com>
2025-06-21 17:11:22 +0200

    evl/net: add support for network interface taps
    Feed the in-band network interface taps with ingress traffic we
    accept, and egress traffic we produce. The scheme works with or
    without oob support from the NIC device the netstack deals with.
    
    This addition enables common traffic sniffers to capture all traffic
    going through EVL netstack (wireshark, tcpdump and friends).
    
    Signed-off-by: Philippe Gerum <philippe.gerum@exail.com>

---
+++ kernel/evl/net/tap.c
@@ -0,0 +1,114 @@
+/*
+ * SPDX-License-Identifier: GPL-2.0
+ *
+ * Copyright (C) 2025 Philippe Gerum  <rpm@xenomai.org>
+ */
+
+#include <linux/inband_work.h>
+#include <linux/netdevice.h>
+#include <linux/skbuff.h>
+#include <evl/net/skb.h>
+#include <evl/net/tap.h>
+
+#define EVL_TAP_RING_SIZE  2048
+
+struct evl_net_tap_data {
+	struct net_device *dev;
+	struct sk_buff *skb;
+};
+
+static void feed_tap_in(struct net_device *dev, struct sk_buff *skb)
+{
+	/*
+	 * The buffer headers are reset and untagged from VLAN bits if
+	 * any as well (see netif_deliver_oob()).
+	 */
+	dev_queue_recv_nit(skb, dev);
+}
+
+static void deliver_input_nit(struct evl_net_tap_data *data)
+{
+	feed_tap_in(data->dev, data->skb);
+	evl_net_free_skb(data->skb);
+}
+
+INBAND_BATCH_WORK(
+	evl_net_tap_in, deliver_input_nit,
+	struct evl_net_tap_data, EVL_TAP_RING_SIZE
+);
+
+static DECLARE_INBAND_BATCH_WORK(evl_net_tap_in, tap_in);
+
+void evl_net_tap_in(struct net_device *dev, struct sk_buff *skb)
+{
+	struct evl_net_tap_data in;
+	struct sk_buff *qskb;
+
+	/*
+	 * If @dev is not oob-capable, we are already running in-band,
+	 * feed the nits via the in-band call immediately.
+	 */
+	if (running_inband()) {
+		feed_tap_in(dev, skb);
+		return;
+	}
+
+	/*
+	 * The guarantee is that we feed taps only with skbs we did
+	 * accept for oob handling (see netif_deliver_oob()),
+	 * therefore evl_net_clone_skb() is by design able to deal
+	 * with any type of buffers the oob stack accepts.
+	 */
+	qskb = evl_net_clone_skb(skb);
+	/* Failing to feed taps not considered harmful. */
+	if (unlikely(!qskb))
+		return;
+
+	/*
+	 * Since we cloned the skb using a buffer from the device
+	 * pool, that device won't vanish until the buffer is released
+	 * (see wmem crossing).
+	 */
+	in.dev = dev;
+	in.skb = qskb;
+	queue_evl_net_tap_in(&tap_in, &in);
+}
+
+static void feed_tap_out(struct net_device *dev, struct sk_buff *skb)
+{
+	dev_queue_xmit_nit(skb, dev);
+}
+
+static void deliver_output_nit(struct evl_net_tap_data *data)
+{
+	feed_tap_out(data->dev, data->skb);
+	evl_net_free_skb(data->skb);
+}
+
+INBAND_BATCH_WORK(
+	evl_net_tap_out, deliver_output_nit,
+	struct evl_net_tap_data, EVL_TAP_RING_SIZE
+);
+
+static DECLARE_INBAND_BATCH_WORK(evl_net_tap_out, tap_out);
+
+void evl_net_tap_out(struct net_device *dev, struct sk_buff *skb)
+{
+	struct evl_net_tap_data out;
+	struct sk_buff *qskb;
+
+	qskb = evl_net_clone_skb(skb);
+	/* Failing to feed taps not considered harmful. */
+	if (unlikely(!qskb))
+		return;
+
+	out.dev = dev;
+	out.skb = qskb;
+	queue_evl_net_tap_out(&tap_out, &out);
+}
+
+void evl_net_init_taps(void)
+{
+	init_evl_net_tap_in(&tap_in);
+	init_evl_net_tap_out(&tap_out);
+}
