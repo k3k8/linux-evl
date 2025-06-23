40bfd1f120da1a3107dbd44216782a7ce4af5729
Philippe Gerum <philippe.gerum@exail.com>
2025-06-23 12:05:14 +0200

    evl/net: dev: sanitize oob port activation/deactivation
    The former interface EVL_SOCKIOC_{ACTIVATE/DEACTIVATE} simply could
    not work, since one cannot bind an oob-enabled device which could be
    returned by proto->getif() for activating it, without making that
    device oob-enabled first.. Classic chicken-and-egg problem.
    
    Rework the logic entirely, channeling request to open an oob port to
    the 'net' root device, then use a fildes obtained on such port to
    apply device-targeted requests, such as disabling it.
    
    Signed-off-by: Philippe Gerum <philippe.gerum@exail.com>

--- include/uapi/evl/net/net-abi.h
+++ include/uapi/evl/net/net-abi.h
@@ -13,11 +13,21 @@
 
 #define EVL_NET_IOCBASE  0xf0
 
+/* Turn on the oob port of a given device. */
+struct evl_net_devparams {
+	__u64 name_ptr;		/* (const char __user *name) */
+	__u64 poolsz;
+	__u64 bufsz;
+	__s32 fd;		/* [out] */
+};
+
+/* Get fildes on an open oob port. */
 struct evl_net_devfd {
 	__u64 name_ptr;		/* (const char __user *name) */
-	__u32 fd;
+	__s32 fd;		/* [out] */
 };
 
 #define EVL_NET_GETDEVFD	_IOWR(EVL_NETDEV_IOCBASE, 0, struct evl_net_devfd)
+#define EVL_NET_OPENPORT	_IOWR(EVL_NETDEV_IOCBASE, 1, struct evl_net_devparams)
 
 #endif /* !_EVL_UAPI_NET_NET_ABI_H */
