504833493b182b95423b06662f6c7811fc0f5bb4
Philippe Gerum <philippe.gerum@exail.com>
2025-07-01 12:38:55 +0200

    net: dovetail: add hook to log RX allocation failures
    The out-of-band core needs to export to oob-capable drivers an
    interface for reporting allocation failures on the RX path.
    
    Provide a placeholder for netif_rx_nomem_oob() the core should
    substitute its own implementation to.
    
    Signed-off-by: Philippe Gerum <philippe.gerum@exail.com>

--- include/linux/netdevice.h
+++ include/linux/netdevice.h
@@ -4472,6 +4472,7 @@ int napi_poll_oob(struct napi_struct *n, struct list_head *repoll);
 void napi_schedule_oob(struct napi_struct *n);
 bool netif_deliver_oob(struct sk_buff *skb);
 void netif_schedule_oob(struct net_device *dev);
+void netif_rx_nomem_oob(struct net_device *dev);
 void netif_tx_lock_oob(struct netdev_queue *txq);
 void netif_tx_unlock_oob(struct netdev_queue *txq);
 void netif_tx_wake_oob(struct netdev_queue *txq); /* rcu_read locked */
@@ -4518,6 +4519,11 @@ static inline bool netif_receive_oob(struct sk_buff *skb)
 
 static inline void netif_schedule_oob(struct net_device *dev)
 {
+}
+
+static inline void netif_rx_nomem_oob(struct net_device *dev)
+{
+	BUG();
 }
 
 static inline bool netif_oob_diversion(const struct net_device *dev)
