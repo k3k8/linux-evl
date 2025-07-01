d61440212b422c766a75990f04e6cc174c1bd122
Philippe Gerum <philippe.gerum@exail.com>
2025-07-01 12:48:14 +0200

    evl/net: export hooks visible from NIC drivers
    Signed-off-by: Philippe Gerum <philippe.gerum@exail.com>

--- kernel/evl/net/output.c
+++ kernel/evl/net/output.c
@@ -282,13 +282,13 @@ void netif_tx_lock_oob(struct netdev_queue *txq) /* oob or in-band */
 {
 	evl_lock_stax(&txq->oob.tx_lock);
 }
-EXPORT_SYMBOL_GPL(netif_tx_lock_oob);
+EXPORT_SYMBOL(netif_tx_lock_oob);
 
 void netif_tx_unlock_oob(struct netdev_queue *txq) /* oob or in-band */
 {
 	evl_unlock_stax(&txq->oob.tx_lock);
 }
-EXPORT_SYMBOL_GPL(netif_tx_unlock_oob);
+EXPORT_SYMBOL(netif_tx_unlock_oob);
 
 void __init evl_net_init_tx(void)
 {
