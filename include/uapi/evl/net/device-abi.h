686a8c0ba381c11ce41af640d26ae3d816547094
Philippe Gerum <philippe.gerum@exail.com>
2025-07-01 12:41:59 +0200

    evl/net: stats: track checksum and buffer allocation failures
    Signed-off-by: Philippe Gerum <philippe.gerum@exail.com>

--- include/uapi/evl/net/device-abi.h
+++ include/uapi/evl/net/device-abi.h
@@ -20,7 +20,13 @@ struct evl_net_devstat {
 	__u32 skb_size;
 	__u32 skb_free;
 	__u32 skb_total;
-	__u8 oob_capable:1;	/* Driver is oob_capable */
+	__u32 csum_errors;
+	__u32 rx_nomem;
+	__u32 tx_nomem;
+	union {
+		__u32 oob_capable:1;	/* Driver is oob_capable */
+		__u32 __flags;
+	};
 };
 
 #define EVL_NDEVIOC_SETRXEBPF	_IOW(EVL_NETDEV_IOCBASE, 0, __s32 /* fd */)
