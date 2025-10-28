71af6fad239b2084edc469f201f2bca8d6543c11
Philippe Gerum <rpm@xenomai.org>
2025-10-28 10:21:00 +0100

    evl/net: qdisc: introduce .flush() operation
    We need a way to flush the buffers maintained by a queuing discipline.
    
    Signed-off-by: Philippe Gerum <rpm@xenomai.org>

--- include/evl/net/skb.h
+++ include/evl/net/skb.h
@@ -43,6 +43,8 @@ void evl_net_init_skb_queue(struct evl_net_skb_queue *txq);
 
 void evl_net_destroy_skb_queue(struct evl_net_skb_queue *txq);
 
+void evl_net_flush_skb_queue(struct evl_net_skb_queue *skbq);
+
 void evl_net_add_skb_queue(struct evl_net_skb_queue *skbq,
 			struct sk_buff *skb);
 
