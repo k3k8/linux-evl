ffb9c57a44f4c7da692d58c99b6f7f97bd059f63
Philippe Gerum <rpm@xenomai.org>
2025-11-07 16:56:50 +0100

    net: dovetail: hold sock lock across sock_oob_bind()
    Signed-off-by: Philippe Gerum <rpm@xenomai.org>

--- net/packet/af_packet.c
+++ net/packet/af_packet.c
@@ -3186,7 +3186,7 @@ static int packet_release(struct socket *sock)
  *	Attach a packet hook.
  */
 
-static int packet_do_bind(struct sock *sk, const char *name, int ifindex,
+static int packet_do_bind_unlocked(struct sock *sk, const char *name, int ifindex,
 			  __be16 proto)
 {
 	struct packet_sock *po = pkt_sk(sk);
@@ -3195,7 +3195,6 @@ static int packet_do_bind(struct sock *sk, const char *name, int ifindex,
 	bool need_rehook;
 	int ret = 0;
 
-	lock_sock(sk);
 	spin_lock(&po->bind_lock);
 	if (!proto)
 		proto = po->num;
@@ -3272,7 +3271,6 @@ static int packet_do_bind(struct sock *sk, const char *name, int ifindex,
 out_unlock:
 	rcu_read_unlock();
 	spin_unlock(&po->bind_lock);
-	release_sock(sk);
 	return ret;
 }
 
@@ -3285,6 +3283,7 @@ static int packet_bind_spkt(struct socket *sock, struct sockaddr *uaddr,
 {
 	struct sock *sk = sock->sk;
 	char name[sizeof(uaddr->sa_data_min) + 1];
+	int ret;
 
 	/*
 	 *	Check legality
@@ -3298,7 +3297,13 @@ static int packet_bind_spkt(struct socket *sock, struct sockaddr *uaddr,
 	memcpy(name, uaddr->sa_data, sizeof(uaddr->sa_data_min));
 	name[sizeof(uaddr->sa_data_min)] = 0;
 
-	return packet_do_bind(sk, name, 0, 0);
+	lock_sock(sk);
+	ret = packet_do_bind_unlocked(sk, name, 0, 0);
+	if (!ret && sock_oob_capable(sk->sk_socket))
+		ret = sock_oob_bind(sk, uaddr, addr_len);
+	release_sock(sk);
+
+	return ret;
 }
 
 static int packet_bind(struct socket *sock, struct sockaddr *uaddr, int addr_len)
@@ -3316,9 +3321,11 @@ static int packet_bind(struct socket *sock, struct sockaddr *uaddr, int addr_len
 	if (sll->sll_family != AF_PACKET)
 		return -EINVAL;
 
-	ret = packet_do_bind(sk, NULL, sll->sll_ifindex, sll->sll_protocol);
-	if (sock_oob_capable(sk->sk_socket))
+	lock_sock(sk);
+	ret = packet_do_bind_unlocked(sk, NULL, sll->sll_ifindex, sll->sll_protocol);
+	if (!ret && sock_oob_capable(sk->sk_socket))
 		ret = sock_oob_bind(sk, uaddr, addr_len);
+	release_sock(sk);
 
 	return ret;
 }
