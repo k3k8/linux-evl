a5ed1a94b40c6e4fcc27f9446fe33a9620381b24
Philippe Gerum <rpm@xenomai.org>
2025-11-09 21:34:29 +0100

    evl/net: input: drop netrx_queue
    We have no more users for this.
    
    Signed-off-by: Philippe Gerum <rpm@xenomai.org>

--- include/evl/net/input.h
+++ include/evl/net/input.h
@@ -16,31 +16,14 @@ struct evl_net_handler {
 	void (*ingress)(struct sk_buff *skb);
 };
 
-struct evl_net_rxqueue {
-	u32 hkey;
-	struct hlist_node hash;
-	struct list_head subscribers;
-	evl_spinlock_t lock;
-	struct list_head next;
-};
-
 void evl_net_do_rx(void *arg);
 
 void evl_net_receive(struct sk_buff *skb,
 		struct evl_net_handler *handler);
 
-struct evl_net_rxqueue *evl_net_alloc_rxqueue(u32 hkey);
-
-void evl_net_free_rxqueue(struct evl_net_rxqueue *rxq);
-
 bool evl_net_ether_accept(struct sk_buff *skb);
 
 bool evl_net_ether_accept_vlan(struct sk_buff *skb);
-
-static inline bool evl_net_rxqueue_active(struct evl_net_rxqueue *rxq)
-{
-	return list_empty(&rxq->subscribers);
-}
 
 ssize_t evl_net_store_vlans(const char *buf, size_t len);
 
