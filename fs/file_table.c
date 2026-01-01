70deeb30661d2c688f81c6a0120c6ca01dadbda8
Philippe Gerum <rpm@xenomai.org>
2026-01-01 18:48:14 +0100

    fs: dovetail: enable fget(), fput() for oob callers
    This provides RCU-based file lookups to out-of-band callers (mostly)
    for free.
    
    Signed-off-by: Philippe Gerum <rpm@xenomai.org>

--- fs/file_table.c
+++ fs/file_table.c
@@ -518,9 +518,30 @@ void flush_delayed_fput(void)
 }
 EXPORT_SYMBOL_GPL(flush_delayed_fput);
 
+static void schedule_delayed_fput(struct file *file)
+{
+	if (llist_add(&file->f_llist, &delayed_fput_list))
+		schedule_delayed_work(&delayed_fput_work, 1);
+}
+
+#ifdef CONFIG_DOVETAIL
+static void ____fput_irq(struct irq_work *irq_work)
+{
+	schedule_delayed_fput(container_of(irq_work, struct file, f_irq_work));
+}
+#endif
+
 static void __fput_deferred(struct file *file)
 {
 	struct task_struct *task = current;
+
+#ifdef CONFIG_DOVETAIL
+	if (running_oob()) {
+		init_irq_work(&file->f_irq_work, ____fput_irq);
+		irq_work_queue(&file->f_irq_work);
+		return;
+	}
+#endif
 
 	if (unlikely(!(file->f_mode & (FMODE_BACKING | FMODE_OPENED)))) {
 		file_free(file);
@@ -538,8 +559,7 @@ static void __fput_deferred(struct file *file)
 		 */
 	}
 
-	if (llist_add(&file->f_llist, &delayed_fput_list))
-		schedule_delayed_work(&delayed_fput_work, 1);
+	schedule_delayed_fput(file);
 }
 
 void fput(struct file *file)
