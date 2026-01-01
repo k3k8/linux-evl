8a27fce978b6044217cb0810af6140d152780c3a
Philippe Gerum <rpm@xenomai.org>
2026-01-01 18:48:14 +0100

    fs: dovetail: enable fget(), fput() for oob callers
    This provides RCU-based file lookups to out-of-band callers (mostly)
    for free.
    
    Signed-off-by: Philippe Gerum <rpm@xenomai.org>

--- include/linux/fs.h
+++ include/linux/fs.h
@@ -46,6 +46,7 @@
 #include <linux/maple_tree.h>
 #include <linux/rw_hint.h>
 #include <linux/file_ref.h>
+#include <linux/irq_work.h>
 #include <linux/unicode.h>
 
 #include <asm/byteorder.h>
@@ -1298,7 +1299,10 @@ struct file {
 	file_ref_t			f_ref;
 	/* --- cacheline 3 boundary (192 bytes) --- */
 #ifdef CONFIG_DOVETAIL
+	/* a pointer to some extended context. */
 	void			       *f_oob_ctx;
+	/* for fput() deferral from oob to in-band. */
+	struct irq_work			f_irq_work;
 #endif
 } __randomize_layout
   __attribute__((aligned(4)));	/* lest something weird decides that 2 is OK */
