bd8f60f4201ab35239a4973967428ed70111ff31
Philippe Gerum <rpm@xenomai.org>
2026-01-02 19:39:59 +0100

    evl: drop redundant placeholders for CONFIG_EVL=n
    Signed-off-by: Philippe Gerum <rpm@xenomai.org>

--- include/asm-generic/evl/poll.h
+++ include/asm-generic/evl/poll.h
@@ -2,8 +2,6 @@
 #ifndef _ASM_GENERIC_EVL_POLL_H
 #define _ASM_GENERIC_EVL_POLL_H
 
-#ifdef CONFIG_EVL
-
 #include <evl/poll_head.h>
 
 /*
@@ -37,23 +35,5 @@ void init_oob_poll_queue(struct oob_poll_queue *pwq)
 
 #define poll_wait_oob(__pwq, __wait)	\
 	evl_poll_watch(&(__pwq)->head, __wait, NULL)
-
-#else
-
-struct oob_poll_wait {
-};
-
-struct oob_poll_queue {
-};
-
-static inline void init_oob_poll_queue(struct oob_poll_queue *pwq) { }
-
-#define poll_signal_oob(__pwq, __mask)	\
-	do { (void)(__pwq), (void)(__mask); } while (0)
-
-#define poll_wait_oob(__pwq, __wait)	\
-	do { (void)(__pwq), (void)(__wait); } while (0)
-
-#endif	/* !CONFIG_EVL */
 
 #endif /* !_ASM_GENERIC_EVL_POLL_H */
--- include/asm-generic/evl/thread_info.h
+++ include/asm-generic/evl/thread_info.h
@@ -2,8 +2,6 @@
 #ifndef _ASM_GENERIC_EVL_THREAD_INFO_H
 #define _ASM_GENERIC_EVL_THREAD_INFO_H
 
-#ifdef CONFIG_EVL
-
 struct evl_thread;
 struct evl_subscriber;
 
@@ -20,11 +18,5 @@ void evl_init_thread_state(struct oob_thread_state *p)
 	p->subscriber = NULL;
 	p->preempt_count = 0;
 }
-
-#else
-
-struct oob_thread_state { };
-
-#endif	/* !CONFIG_EVL */
 
 #endif /* !_ASM_GENERIC_EVL_THREAD_INFO_H */
