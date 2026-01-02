4c7035921418c973664015e9deb81252ca66e0b0
Philippe Gerum <rpm@xenomai.org>
2026-01-02 11:00:52 +0100

    dovetail: drop {install, uninstall, replace}_inband_fd() hooks
    With the ability to use the fget()/fput() interface from the
    out-of-band stage, the use case for interposing on file table update
    operations has vanished, since this was basically useful for managing
    a shadow file table from that stage.
    
    Let's drop the related hooks entirely.
    
    Signed-off-by: Philippe Gerum <rpm@xenomai.org>

--- fs/file.c
+++ fs/file.c
@@ -491,7 +491,6 @@ static struct fdtable *close_files(struct files_struct * files)
 			if (set & 1) {
 				struct file *file = fdt->fd[i];
 				if (file) {
-					uninstall_inband_fd(i, file, files);
 					filp_close(file, files);
 					cond_resched();
 				}
@@ -667,7 +666,6 @@ static void noinline fd_install_slowpath(unsigned int fd, struct file *file)
 	fdt = files_fdtable(files);
 	VFS_BUG_ON(rcu_access_pointer(fdt->fd[fd]) != NULL);
 	rcu_assign_pointer(fdt->fd[fd], file);
-	install_inband_fd(fd, file, files);
 	spin_unlock(&files->file_lock);
 }
 
@@ -698,7 +696,6 @@ void fd_install(unsigned int fd, struct file *file)
 	fdt = rcu_dereference_sched(files->fdt);
 	VFS_BUG_ON(rcu_access_pointer(fdt->fd[fd]) != NULL);
 	rcu_assign_pointer(fdt->fd[fd], file);
-	install_inband_fd(fd, file, files);
 	rcu_read_unlock_sched();
 }
 
@@ -730,7 +727,6 @@ struct file *file_close_fd_locked(struct files_struct *files, unsigned fd)
 	if (file) {
 		rcu_assign_pointer(fdt->fd[fd], NULL);
 		__put_unused_fd(files, fd);
-		uninstall_inband_fd(fd, file, files);
 	}
 	return file;
 }
@@ -912,7 +908,6 @@ void do_close_on_exec(struct files_struct *files)
 				continue;
 			rcu_assign_pointer(fdt->fd[fd], NULL);
 			__put_unused_fd(files, fd);
-			uninstall_inband_fd(fd, file, files);
 			spin_unlock(&files->file_lock);
 			filp_close(file, files);
 			cond_resched();
@@ -1349,7 +1344,6 @@ __releases(&files->file_lock)
 	get_file(file);
 	rcu_assign_pointer(fdt->fd[fd], file);
 	__set_open_fd(fd, fdt, flags & O_CLOEXEC);
-	replace_inband_fd(fd, file, files);
 	spin_unlock(&files->file_lock);
 
 	if (tofree)
