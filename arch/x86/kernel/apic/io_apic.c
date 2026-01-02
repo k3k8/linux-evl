4bc96e7cac056cc15588aef770f9a5b4cc5da574
Philippe Gerum <rpm@xenomai.org>
2026-01-02 10:35:53 +0100

    x86: irq_pipeline: include missing definitions for DOVETAIL=n
    Signed-off-by: Philippe Gerum <rpm@xenomai.org>

--- arch/x86/kernel/apic/io_apic.c
+++ arch/x86/kernel/apic/io_apic.c
@@ -34,6 +34,7 @@
 #include <linux/mm.h>
 #include <linux/interrupt.h>
 #include <linux/irq.h>
+#include <linux/irqstage.h>
 #include <linux/init.h>
 #include <linux/delay.h>
 #include <linux/sched.h>
