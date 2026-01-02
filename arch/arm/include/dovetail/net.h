ddc96a2587eb619b4d9f324eef9be1470b0a1ce4
Philippe Gerum <rpm@xenomai.org>
2026-01-02 19:38:41 +0100

    arm/evl: stop duplicating placeholders with CONFIG_EVL=n
    Signed-off-by: Philippe Gerum <rpm@xenomai.org>

--- arch/arm/include/dovetail/net.h
+++ arch/arm/include/dovetail/net.h
@@ -2,6 +2,10 @@
 #ifndef _EVL_DOVETAIL_NET_H
 #define _EVL_DOVETAIL_NET_H
 
+#ifdef CONFIG_EVL
 #include <asm-generic/evl/net.h>
+#else
+#include_next <dovetail/net.h>
+#endif
 
 #endif /* !_EVL_DOVETAIL_NET_H */
