2db05b13a2e1a75cefd3738ac55f4c3efa9bd173
Philippe Gerum <rpm@xenomai.org>
2026-01-02 11:00:52 +0100

    dovetail: drop {install, uninstall, replace}_inband_fd() hooks
    With the ability to use the fget()/fput() interface from the
    out-of-band stage, the use case for interposing on file table update
    operations has vanished, since this was basically useful for managing
    a shadow file table from that stage.
    
    Let's drop the related hooks entirely.
    
    Signed-off-by: Philippe Gerum <rpm@xenomai.org>

--- fs/file.c
+++ fs/file.c
@@ -422,7 +422,6 @@ static struct fdtable *close_files(struct files_struct * files)
 			if (set & 1) {
 				struct file * file = xchg(&fdt->fd[i], NULL);
 				if (file) {
-					uninstall_inband_fd(i, file, files);
 					filp_close(file, files);
 					cond_resched();
 				}
@@ -609,7 +608,6 @@ void fd_install(unsigned int fd, struct file *file)
 		fdt = files_fdtable(files);
 		BUG_ON(fdt->fd[fd] != NULL);
 		rcu_assign_pointer(fdt->fd[fd], file);
-		install_inband_fd(fd, file, files);
 		spin_unlock(&files->file_lock);
 		return;
 	}
@@ -618,7 +616,6 @@ void fd_install(unsigned int fd, struct file *file)
 	fdt = rcu_dereference_sched(files->fdt);
 	BUG_ON(fdt->fd[fd] != NULL);
 	rcu_assign_pointer(fdt->fd[fd], file);
-	install_inband_fd(fd, file, files);
 	rcu_read_unlock_sched();
 }
 
@@ -650,7 +647,6 @@ struct file *file_close_fd_locked(struct files_struct *files, unsigned fd)
 	if (file) {
 		rcu_assign_pointer(fdt->fd[fd], NULL);
 		__put_unused_fd(files, fd);
-		uninstall_inband_fd(fd, file, files);
 	}
 	return file;
 }
@@ -830,7 +826,6 @@ void do_close_on_exec(struct files_struct *files)
 				continue;
 			rcu_assign_pointer(fdt->fd[fd], NULL);
 			__put_unused_fd(files, fd);
-			uninstall_inband_fd(fd, file, files);
 			spin_unlock(&files->file_lock);
 			filp_close(file, files);
 			cond_resched();
@@ -1258,7 +1253,6 @@ __releases(&files->file_lock)
 		__set_close_on_exec(fd, fdt);
 	else
 		__clear_close_on_exec(fd, fdt);
-	replace_inband_fd(fd, file, files);
 	spin_unlock(&files->file_lock);
 
 	if (tofree)
