3e5e9a041a5cf82855aa3868c099f9a60f63d1dc
Philippe Gerum <rpm@xenomai.org>
2026-01-02 19:38:48 +0100

    x86/evl: stop duplicating placeholders with CONFIG_EVL=n
    Signed-off-by: Philippe Gerum <rpm@xenomai.org>

--- arch/x86/include/dovetail/poll.h
+++ arch/x86/include/dovetail/poll.h
@@ -2,6 +2,10 @@
 #ifndef _EVL_DOVETAIL_POLL_H
 #define _EVL_DOVETAIL_POLL_H
 
+#ifdef CONFIG_EVL
 #include <asm-generic/evl/poll.h>
+#else
+#include_next <dovetail/poll.h>
+#endif
 
 #endif /* !_EVL_DOVETAIL_POLL_H */
