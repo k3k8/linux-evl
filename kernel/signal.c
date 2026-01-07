a64527a9f851e5ac88cdfa5a885488adc085938e
Philippe Gerum <rpm@xenomai.org>
2026-01-07 09:56:26 +0100

    ptrace: dovetail: move call sites of PTSTOP/PTCONT events
    It is better to invoke Dovetail callouts without holding any in-band
    locks, along with having the calling context fully stable and fixed
    up.
    
    To this end, move the call sites of the INBAND_TASK_PTSTOP and
    INBAND_TASK_PTCONT events around the schedule() call in ptrace_stop().
    
    Signed-off-by: Philippe Gerum <rpm@xenomai.org>

--- kernel/signal.c
+++ kernel/signal.c
@@ -2257,8 +2257,6 @@ static int ptrace_stop(int exit_code, int why, unsigned long message,
 		spin_lock_irq(&current->sighand->siglock);
 	}
 
-	inband_ptstop_notify();
-
 	/*
 	 * After this point ptrace_signal_wake_up or signal_wake_up
 	 * will clear TASK_TRACED if ptrace_unlink happens or a fatal
@@ -2363,10 +2361,10 @@ static int ptrace_stop(int exit_code, int why, unsigned long message,
 	cgroup_enter_frozen();
 	if (!IS_ENABLED(CONFIG_PREEMPT_RT))
 		preempt_enable_no_resched();
+	inband_ptstop_notify();
 	schedule();
-	cgroup_leave_frozen(true);
-
 	inband_ptcont_notify();
+	cgroup_leave_frozen(true);
 
 	/*
 	 * We are back.  Now reacquire the siglock before touching
