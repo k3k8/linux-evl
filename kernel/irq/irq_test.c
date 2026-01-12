02c80085cadad5f0ca04fc8d89a1b9bfc025b1d9
Philippe Gerum <rpm@xenomai.org>
2026-01-12 16:55:18 +0100

    genirq: irq_pipeline: mark test irqchip as pipeline-safe
    Signed-off-by: Philippe Gerum <rpm@xenomai.org>

--- kernel/irq/irq_test.c
+++ kernel/irq/irq_test.c
@@ -38,7 +38,7 @@ static struct irq_chip fake_irq_chip = {
 	.irq_mask       = noop,
 	.irq_unmask     = noop,
 	.irq_set_affinity = noop_affinity,
-	.flags          = IRQCHIP_SKIP_SET_WAKE,
+	.flags          = IRQCHIP_SKIP_SET_WAKE|IRQCHIP_PIPELINE_SAFE,
 };
 
 static int irq_test_setup_fake_irq(struct kunit *test, struct irq_affinity_desc *affd)
