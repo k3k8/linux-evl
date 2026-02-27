9fa4cf951c2827b3b5fca2bba809feadb239e287
Philippe Gerum <rpm@xenomai.org>
2026-02-27 10:29:13 +0100

    evl/net: skb: fix free buffer accounting
    page_pool_put_full_netmem() (nap_pp_put_page->page_pull_full_netmem)
    might defer a release request until all users have dropped their
    reference on the target page, in which case we the operation should
    not account for an actual release, otherwise the reported free buffer
    count may become negative.
    
    This issue is easily observed when a network tap is active on a
    high-traffic device, which decreases the odds of page sharing between
    users.
    
    NOTE: the fixed implementation directly accesses the free buffer count
    in the page cache because having a general page pool interface for
    this purpose would be unsuitable. As a matter of fact, although oob
    page pools restrict their page stock to the fast cache, regular pools
    don't and may grow this stock dynamically, so we can't have a unified
    interface regardless of the pool type.
    
    Signed-off-by: Philippe Gerum <rpm@xenomai.org>

--- kernel/evl/net/skb.c
+++ kernel/evl/net/skb.c
@@ -106,11 +106,8 @@ static struct page *alloc_bufpage(struct net_device *dev,
 		raw_spin_lock_irqsave(&est->tx_wait.wchan.lock, flags);
 
 		page = page_pool_dev_alloc_pages(est->tx_pages);
-		if (likely(page)) {
-			/* Feed statistics under lock. */
-			evl_counter_inc_careful(&est->stats.pool_alloc_count);
+		if (likely(page))
 			break;
-		}
 
 		if (timeout == EVL_NONBLOCK) {
 			page = ERR_PTR(-EWOULDBLOCK);
@@ -252,9 +249,6 @@ static void __free_evl_skb(struct sk_buff *skb, struct net_device *dev)
 	 * device we are releasing the page to.
 	 */
 	raw_spin_lock_irqsave(&est->tx_wait.wchan.lock, flags);
-
-	/* Feed statistics under lock. */
-	evl_counter_inc_careful(&est->stats.pool_release_count);
 
 	if (evl_wait_active(&est->tx_wait))
 		evl_wake_up_head(&est->tx_wait);
