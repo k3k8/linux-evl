6393f6b8674ff31841dc77a87e2ac6999e4da7c5
Philippe Gerum <rpm@xenomai.org>
2026-02-27 10:29:13 +0100

    evl/net: skb: fix free buffer accounting
    page_pool_put_full_netmem() (nap_pp_put_page->page_pull_full_netmem)
    might defer a release request until all users have dropped their
    reference on the target page, in which case we the operation should
    not account for an actual release, otherwise the reported free buffer
    count may become negative.
    
    This issue is easily observed when a network tap is active on a
    high-traffic device, which decreases the odds of page sharing between
    users.
    
    NOTE: the fixed implementation directly accesses the free buffer count
    in the page cache because having a general page pool interface for
    this purpose would be unsuitable. As a matter of fact, although oob
    page pools restrict their page stock to the fast cache, regular pools
    don't and may grow this stock dynamically, so we can't have a unified
    interface regardless of the pool type.
    
    Signed-off-by: Philippe Gerum <rpm@xenomai.org>

--- include/asm-generic/evl/netdevice.h
+++ include/asm-generic/evl/netdevice.h
@@ -59,8 +59,6 @@ struct evl_netdev_state {
 		evl_counter64 tx_bytes;
 		evl_counter32 rx_nomem;
 		evl_counter32 tx_nomem;
-		evl_counter32 pool_alloc_count;
-		evl_counter32 pool_release_count;
 		evl_counter32 csum_errors;
 	} stats;
 	/* Count of oob ports referring to this device. */
