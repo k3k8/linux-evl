384f5d54ae3917c5ada1dd7690412ee932674877
Philippe Gerum <rpm@xenomai.org>
2026-03-04 21:23:01 +0100

    evl/uapi: introduce generic header in shared element states
    As prep work for the introduction of the POSIX interface (aka
    libcobalt) which is going to require mere fundle-based requests to
    monitors and threads (instead of fd-based), add a generic header to
    the shared states of those elements, containing the information we
    need to carry out such requests.
    
    At this chance, rename some internal types exported by the uAPI to
    give a clear hint about their nature (i.e. part of the ABI, not the
    API).
    
    Signed-off-by: Philippe Gerum <rpm@xenomai.org>

--- kernel/evl/syscall.c
+++ kernel/evl/syscall.c
@@ -286,7 +286,7 @@ static int do_oob_syscall(struct irq_stage *stage, struct pt_regs *regs,
 do_stop:
 	/* Update the stats and user visible info. */
 	evl_opt_counter_inc(&curr->stat.sc);
-	evl_sync_uwindow(curr);
+	evl_sync_sstate(curr);
 
 	trace_evl_oob_sysexit(syscall_get_return_value(tsk, regs));
 
@@ -382,7 +382,7 @@ done:
 		curr->local_info &= ~EVL_T_IGNOVR;
 
 	evl_opt_counter_inc(&curr->stat.sc);
-	evl_sync_uwindow(curr);
+	evl_sync_sstate(curr);
 
 	trace_evl_inband_sysexit(syscall_get_return_value(tsk, regs));
 
