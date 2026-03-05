d10229c326afbde9d92d76c5e8f6bb74e0214318
Philippe Gerum <rpm@xenomai.org>
2026-03-05 19:49:33 +0100

    evl/uapi: bump to ABI 44
    This ABI upgrade covers all the changes laying the groundwork for the
    upcoming POSIX support (aka libcobalt).
    
    Signed-off-by: Philippe Gerum <rpm@xenomai.org>

--- include/uapi/evl/control-abi.h
+++ include/uapi/evl/control-abi.h
@@ -10,14 +10,14 @@
 #include <linux/types.h>
 
 /* Earliest ABI level we support. */
-#define EVL_ABI_BASE   43
+#define EVL_ABI_BASE   44
 /*
  * Current/latest ABI level we support. We may decouple the base and
  * current ABI levels by providing backward compatibility from the
  * latter to the former. CAUTION: a litteral value is required for the
  * current ABI definition (scripts reading this may be naive).
  */
-#define EVL_ABI_LEVEL  43
+#define EVL_ABI_LEVEL  44
 
 #define EVL_CONTROL_DEV  "/dev/evl/control"
 
