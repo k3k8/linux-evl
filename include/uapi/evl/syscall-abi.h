7a170a7e2a8148a2749fc25eee08ba51983d8731
Philippe Gerum <rpm@xenomai.org>
2026-03-11 16:24:37 +0100

    evl/syscall: introduce [i]functl requests
    Owned elements have no file binding, therefore are referenced by
    fundles instead of file descriptors. We need a way to issue system
    calls targeting these elements without resorting to file I/O requests.
    
    To this end, we introduce the new '[i]functl' system calls (short for
    "fundle-based control request") which is an ioctl-like syscall
    accepting a fundle to identify the target element.
    
    The in-band switching call ('ifunctl') is the counterpart of ioctl()
    for fundle-based requests, while the regular 'functl' call is the
    counterpart of oob_ioctl().
    
    At the moment, we have functl requests for the thread and monitor
    elements only. Provide the necessary wrapping to the relevant
    [oob_]ioctl() handlers.
    
    Signed-off-by: Philippe Gerum <rpm@xenomai.org>

--- include/uapi/evl/syscall-abi.h
+++ include/uapi/evl/syscall-abi.h
@@ -10,7 +10,9 @@
 #define sys_evl_read	0	/* oob_read() */
 #define sys_evl_write	1	/* oob_write() */
 #define sys_evl_ioctl	2	/* oob_ioctl() */
+#define sys_evl_functl	3	/* oob_funcall() */
+#define sys_evl_ifunctl	4	/* inband_funcall() */
 
-#define NR_EVL_SYSCALLS 3
+#define NR_EVL_SYSCALLS 5
 
 #endif /* !_EVL_UAPI_SYSCALL_ABI_H */
