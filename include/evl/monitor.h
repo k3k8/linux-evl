125fcccdf106ea3ede15165babea56015b6b0781
Philippe Gerum <rpm@xenomai.org>
2026-03-11 16:24:37 +0100

    evl/syscall: introduce [i]functl requests
    Owned elements have no file binding, therefore are referenced by
    fundles instead of file descriptors. We need a way to issue system
    calls targeting these elements without resorting to file I/O requests.
    
    To this end, we introduce the new '[i]functl' system calls (short for
    "fundle-based control request") which is an ioctl-like syscall
    accepting a fundle to identify the target element.
    
    The in-band switching call ('ifunctl') is the counterpart of ioctl()
    for fundle-based requests, while the regular 'functl' call is the
    counterpart of oob_ioctl().
    
    At the moment, we have functl requests for the thread and monitor
    elements only. Provide the necessary wrapping to the relevant
    [oob_]ioctl() handlers.
    
    Signed-off-by: Philippe Gerum <rpm@xenomai.org>

--- include/evl/monitor.h
+++ include/evl/monitor.h
@@ -7,8 +7,50 @@
 #ifndef _EVL_MONITOR_H
 #define _EVL_MONITOR_H
 
+#include <linux/list.h>
+#include <linux/wait.h>
+#include <linux/irq_work.h>
+#include <evl/wait.h>
 #include <evl/thread.h>
+#include <evl/mutex.h>
+#include <evl/poll.h>
+#include <evl/factory.h>
 #include <uapi/evl/monitor-abi.h>
+
+struct evl_monitor {
+	struct evl_element element;
+	struct __evl_monitor_sstate *sstate;
+	int type : 2,
+	    protocol : 4;
+	union {
+		struct {
+			struct evl_mutex mutex;
+			struct list_head events;
+			hard_spinlock_t lock;
+		};
+		struct {
+			/* Out-of-band wait queue. */
+			struct evl_wait_queue wait_queue;
+			/* In-band wait queue (read side). */
+			wait_queue_head_t inband_wait_r;
+			/* In-band wait queue (write side). */
+			wait_queue_head_t inband_wait_w;
+			/* In-band wake up work (read side). */
+			struct irq_work inband_wake_r;
+			/* In-band wake up work (write side). */
+			struct irq_work inband_wake_w;
+			/* Gate (valid during active wait only). */
+			struct evl_monitor *gate;
+			/* Out-of-band poll head. */
+			struct evl_poll_head poll_head;
+			/* in ->events */
+			struct list_head next;
+		};
+	};
+};
+
+long evl_functl_monitor(struct evl_monitor *mon,
+		unsigned int cmd, unsigned long arg);
 
 int evl_signal_monitor_targeted(struct evl_thread *target,
 				int monfd);
