289dd1bb5f86fa2d61da1cba3bbe423014f9d6f4
Philippe Gerum <rpm@xenomai.org>
2026-03-29 19:19:29 +0200

    evl/net: dev: force unbind sockets on device teardown
    Some protocols with active device bindings may want to know when an
    (oob) interface goes down, typically for resetting their internal
    filter logic.
    
    To this end, implement an advisory binding scheme which such protocols
    can invoke to register for notification when an oob port is disabled
    (namely, via the new force_unbind() protocol handler).
    
    Signed-off-by: Philippe Gerum <rpm@xenomai.org>

--- include/asm-generic/evl/netdevice.h
+++ include/asm-generic/evl/netdevice.h
@@ -5,6 +5,7 @@
 #include <linux/list.h>
 #include <linux/rcupdate.h>
 #include <linux/refcount.h>
+#include <linux/mutex.h>
 #include <net/page_pool/types.h>
 #include <evl/wait.h>
 #include <evl/poll.h>
@@ -69,8 +70,10 @@ struct evl_netdev_stats {
 struct oob_netdev_state {
 	struct evl_netdev_state *estate;
 	struct evl_netdev_stats *stats;
+	struct mutex bind_lock;
+	struct list_head bindings; /* evl_socket->next_binding */
 	struct evl_crossing crossing;
-	struct list_head next;
+	struct list_head next;	/* in oob_port_list */
 };
 
 struct oob_netqueue_state {
