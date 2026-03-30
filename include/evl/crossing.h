ff6335c9d53d9515876d08e9e359511f750f732a
Philippe Gerum <rpm@xenomai.org>
2026-03-30 11:49:43 +0200

    evl: crossing: use common refcounting infrastructure
    Signed-off-by: Philippe Gerum <rpm@xenomai.org>

--- include/evl/crossing.h
+++ include/evl/crossing.h
@@ -7,72 +7,74 @@
 #ifndef _EVL_CROSSING_H
 #define _EVL_CROSSING_H
 
-#include <linux/atomic.h>
+#include <linux/refcount.h>
 #include <linux/completion.h>
 #include <linux/irq_work.h>
 
 /*
  * This is a simple synchronization mechanism allowing an in-band
  * caller to pass a point in the code making sure that no concurrent
- * operation which might traverse the same crossing is in flight,
- * whether in-band or out-of-band. Once the crossing is passed, it
- * _must_ be reinitialized.
+ * out-of-band operation which might traverse the same crossing is in
+ * flight.
  *
- * Callers delimit the danger zone by down-ing and up-ing the barrier
- * at the crossing, the in-band code should ask for passing the
- * crossing.
+ * Out-of-band callers delimit the danger zone by down-ing
+ * (evl_down_crossing()) and up-ing (evl_up_crossing()) the barrier at
+ * the crossing. Conversely, the in-band code should ask for passing
+ * the latter (evl_pass_crossing()) to make sure no out-of-band
+ * operation is ongoing on the protected resource.
  *
- * CAUTION: the caller must guarantee that evl_down_crossing() cannot
- * be invoked _after_ evl_pass_crossing() is entered for a given
- * crossing.
+ * CAUTION: the sequence evl_pass_crossing() -> evl_down_crossing() is
+ * _invalid_, the implementation must guarantee that it never happens.
+ * A call to evl_reinit_crossing() must reset the crossing in-between:
+ * evl_pass_crossing() -> evl_reinit_crossing() -> evl_down_crossing().
  */
 
 struct evl_crossing {
-	atomic_t oob_refs;
-	struct completion oob_done;
-	struct irq_work oob_work;
+	refcount_t refs;
+	struct completion passed;
+	struct irq_work irq_work;
 };
 
 static inline void evl_open_crossing(struct irq_work *work)
 {
-	struct evl_crossing *c = container_of(work, struct evl_crossing, oob_work);
-	complete(&c->oob_done);
+	struct evl_crossing *c = container_of(work, struct evl_crossing, irq_work);
+	complete(&c->passed);
 }
 
 static inline void evl_init_crossing(struct evl_crossing *c)
 {
-	atomic_set(&c->oob_refs, 1);
-	init_completion(&c->oob_done);
-	init_irq_work(&c->oob_work, evl_open_crossing);
+	refcount_set(&c->refs, 1);
+	init_completion(&c->passed);
+	init_irq_work(&c->irq_work, evl_open_crossing);
 }
 
 static inline void evl_reinit_crossing(struct evl_crossing *c)
 {
-	atomic_set(&c->oob_refs, 1);
-	reinit_completion(&c->oob_done);
+	refcount_set(&c->refs, 1);
+	reinit_completion(&c->passed);
 }
 
 static inline void evl_down_crossing(struct evl_crossing *c)
 {
-	atomic_inc(&c->oob_refs);
+	refcount_inc(&c->refs);
 }
 
 static inline void evl_up_crossing(struct evl_crossing *c)
 {
 	/* CAUTION: See word of caution in the initial comment. */
 
-	if (atomic_dec_return(&c->oob_refs) == 0) {
+	if (refcount_dec_and_test(&c->refs)) {
 		if (running_inband() && !hard_irqs_disabled())
-			complete(&c->oob_done);
+			complete(&c->passed);
 		else
-			irq_work_queue(&c->oob_work);
+			irq_work_queue(&c->irq_work);
 	}
 }
 
 static inline void evl_pass_crossing(struct evl_crossing *c)
 {
-	if (atomic_dec_return(&c->oob_refs) > 0)
-		wait_for_completion(&c->oob_done);
+	if (!refcount_dec_and_test(&c->refs))
+		wait_for_completion(&c->passed);
 }
 
 #endif /* !_EVL_CROSSING_H */
