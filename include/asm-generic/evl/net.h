476e21e0e2d5523899ac70dddd9824e051eb3d0d
Philippe Gerum <rpm@xenomai.org>
2026-04-10 10:33:00 +0200

    evl/net: dev: narrow VLAN lookups to base device
    There is no point in looking for a VLAN device in the entire network
    namespace when the base device channelling its traffic is known
    upfront.
    
    Rework the implementation so that every oob-enabled upper device is
    directly linked to its base device.
    
    Signed-off-by: Philippe Gerum <rpm@xenomai.org>

--- include/asm-generic/evl/net.h
+++ include/asm-generic/evl/net.h
@@ -82,8 +82,6 @@ struct oob_net_state {
 		/* Cache of active UDP4 receivers. */
 		struct evl_cache udp;
 	} ipv4;
-	/* VLAN devices with active oob ports. */
-	struct list_head oob_vlans;
 };
 
 void net_init_oob_state(struct net *net);
--- kernel/evl/net/net.c
+++ kernel/evl/net/net.c
@@ -33,7 +33,6 @@ void net_init_oob_state(struct net *net)
 {
 	evl_net_init_packet(net);
 	evl_net_init_ipv4(net);
-	INIT_LIST_HEAD(&net->oob.oob_vlans);
 }
 
 /*
