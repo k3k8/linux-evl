cd4f7b4db61ab1db1089b535019aa32df9111dbc
Philippe Gerum <rpm@xenomai.org>
2026-04-10 20:01:47 +0200

    evl/net: ethernet: straighten action rules from accept filters
    We need to tidy up the action rules on incoming packets a bit. To this
    end, the following changes have been applied:
    
    - allow for an eBPF module to accept a VLAN-tagged packet
      (EVL_RX_ACCEPT) leaving us the burden of popping the VLAN header
      provided such VLAN is deemed out-of-band, otherwise the filter would
      be clearly broken and we shall drop the packet.
    
    - drop malformed VLAN packets with invalid headers, don't even try to
      pass them to the in-band stack.
    
    Signed-off-by: Philippe Gerum <rpm@xenomai.org>

--- include/evl/net/input.h
+++ include/evl/net/input.h
@@ -9,6 +9,7 @@
 
 #include <linux/list.h>
 #include <evl/lock.h>
+#include <uapi/evl/net/bpf-abi.h>
 
 struct sk_buff;
 
@@ -21,9 +22,9 @@ void evl_net_do_rx(void *arg);
 void evl_net_receive(struct sk_buff *skb,
 		struct evl_net_handler *handler);
 
-bool evl_net_ether_accept(struct sk_buff *skb);
+enum evl_net_rx_action evl_net_ether_accept(struct sk_buff *skb);
 
-bool evl_net_ether_accept_vlan(struct sk_buff *skb);
+enum evl_net_rx_action evl_net_ether_accept_vlan(struct sk_buff *skb);
 
 ssize_t evl_net_store_vlans(const char *buf, size_t len);
 
