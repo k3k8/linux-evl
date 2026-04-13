ac5a6ab96422623e634d523fff2d4b076e89efb3
Philippe Gerum <rpm@xenomai.org>
2026-04-13 09:35:03 +0000

    evl/net: dev: clarify ioctl interface
    We currently have a clumsly ioctl interface to netdevices, with
    unclear separation of concerns between the global "net" handler and
    the per-device handler.
    
    Fix this by reorganizing the interface as follows:
    
    - the global "net" ioctl handler shall open a netdevice by name,
      whether an oob port is enabled on it or not, returning a file
      descriptor. As a by-product, this allows for querying the base
      device of a VLAN (upper) device for stats and status regardless of
      whether the former has an oob port active, which was not possible
      prior to this change.
    
    - the per-device netdev_ioctl() handler is now in charge of all
      operations on an open netdev, including toggling the oob port
      on/off.
    
    Signed-off-by: Philippe Gerum <rpm@xenomai.org>

--- include/uapi/evl/net/net-abi.h
+++ include/uapi/evl/net/net-abi.h
@@ -13,21 +13,12 @@
 
 #define EVL_NET_IOCBASE  0xf0
 
-/* Turn on the oob port of a given device. */
-struct evl_net_devparams {
-	__u64 name_ptr;		/* (const char __user *name) */
-	__u64 poolsz;
-	__u64 bufsz;
-	__s32 fd;		/* [out] */
-};
-
-/* Get fildes on an open oob port. */
-struct evl_net_devfd {
+/* Get fildes on a network device. */
+struct evl_net_devopen {
 	__u64 name_ptr;		/* (const char __user *name) */
 	__s32 fd;		/* [out] */
 };
 
-#define EVL_NET_GETDEVFD	_IOWR(EVL_NETDEV_IOCBASE, 0, struct evl_net_devfd)
-#define EVL_NET_OPENPORT	_IOWR(EVL_NETDEV_IOCBASE, 1, struct evl_net_devparams)
+#define EVL_NETIOC_DEVOPEN	_IOWR(EVL_NETDEV_IOCBASE, 0, struct evl_net_devopen)
 
 #endif /* !_EVL_UAPI_NET_NET_ABI_H */
