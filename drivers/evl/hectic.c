c00d8451b5df4b08a9f49aeeed4f7a2ec81bb80c
Philippe Gerum <rpm@xenomai.org>
2026-05-08 10:14:11 +0200

    evl/timer: allow running timer on arbitrary clock
    Signed-off-by: Philippe Gerum <rpm@xenomai.org>

--- drivers/evl/hectic.c
+++ drivers/evl/hectic.c
@@ -611,7 +611,7 @@ static int hectic_open(struct inode *inode, struct file *filp)
 	ctx->pause_us = 0;
 
 	init_irq_work(&ctx->wake_utask, rtswitch_utask_waker);
-	evl_init_timer(&ctx->wake_up_delay, timed_wake_up);
+	evl_init_timer(&ctx->wake_up_delay, &evl_mono_clock, timed_wake_up);
 	evl_init_stax(&ctx->stax, 0);
 
 	filp->private_data = ctx;
