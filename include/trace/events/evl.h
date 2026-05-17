815735e18cab14b8bfca012d28221b13e909b7d8
Philippe Gerum <rpm@xenomai.org>
2026-05-17 11:50:05 +0200

    evl/sched: fix enforcement of forcible suspend upon signal
    We have a major issue when it comes to dealing with signals pending
    for forcibly suspended threads , which causes evl_kick_thread() to
    spuriously clear EVL_T_SUSP upon receipt of an in-band signal.
    
    However, we may expect that specific condition to linger enough in
    order to be enforced as soon as the target thread switches back to the
    out-of-band stage, and this obviously won't happen.
    
    For instance, (A) and (B) threads are running oob, (X) may be any
    in-band context:
    
    CPU0                                              CPU1
    ----                                              ----
    
    A: evl_signal_thread(B, SIGxx)                    B: oob_syscall()
       <schedule irq_work>                                  evl_sleep_on()
    A: evl_hold_thread(B, EVL_T_SUSP)                 B: <schedules out>
       evl_schedule()
          send_oob_ipi(CPU1)
    X: irq_work_handler()
          send_sig(B, SIGxx)
             sigwake_event(B, INBAND_TASK_SIGNAL)
    	    evl_kick_thread(B)
                   evl_wakeup_thread(B, EVL_T_PEND)
                   evl_release_thread(B, EVL_T_SUSP)
                   B->info |= EVL_T_KICKED
                   evl_schedule()
                      send_oob_ipi(CPU1)
                                                      X: <ipi-handler>
                                                            ev_schedule()
                                                      B: <resuming oob>
                                                      B: evl_exit_to_user()
                                                            EVL_T_KICKED: evl_switch_inband()
                                                      B: <running in-band>
                                                      B: oob_syscall()
                                                         nop (EVL_T_SUSP was cleared by evl_kicked_thread())
                                                      B: <still running in-band, suspend request lost>
    
    A different bug could bite on a single-CPU scenario as well, e.g. with
    prio(A) > prio(B), we might observe:
    
    CPU0
    ----
    
    B: oob_syscall()
          evl_sleep_on()
    B: <schedules out>
    X: send_sig(B, SIGxx)
          sigwake_event(B, INBAND_TASK_SIGNAL)
             evl_kick_thread(B)
                evl_wakeup_thread(B, EVL_T_PEND) /* B is now fully unblocked */
                evl_release_thread(B, EVL_T_SUSP)
                B->info |= EVL_T_KICKED
                evl_schedule()
                   send_oob_ipi(CPU1)
    <IRQ>: evl_wakeup_thread(A, ...)
    A: <resuming oob>
    A: evl_hold_thread(B, EVL_T_SUSP)
          unblocked + EVL_T_SUSP set -> abort: EVL_T_SUSP ignored
    B: <resuming oob>
    B: evl_exit_to_user()
          EVL_T_KICKED: evl_switch_inband()
    B: <still running in-band>
    
    There is no shortage of possible variations of the scenarios above
    especially if we bring up more complex multi-CPU configurations, all
    of them causing some sort of breakage.
    
    In order to address these issues systematically, this patch breaks an
    ancient invariant, which used to state that a thread with any blocking
    bit set must not be linked to any runqueue.
    
    On the contrary, we now allow a particular set of flags (namely
    EVL_THREAD_LAZY_MASK which includes EVL_T_SUSP) to stick as long as
    necessary in a thread state mask until such thread performs the
    required migration to the in-band stage, keeping it runnable until
    this happens, at which point the suspended state may come into force
    when the thread switches back to the out-of-band stage. IOW, the
    conditions denoted by those flags are now lazily enforced as long as
    EVL_T_KICKED is set, and not cleared when the latter is raised.
    
    At this chance, drop the EVL_T_HALT condition which has no user.
    
    Signed-off-by: Philippe Gerum <rpm@xenomai.org>

--- include/trace/events/evl.h
+++ include/trace/events/evl.h
@@ -410,26 +410,23 @@ TRACE_EVENT(evl_hold_thread,
 );
 
 TRACE_EVENT(evl_release_thread,
-	TP_PROTO(struct evl_thread *thread, int mask, int info),
-	TP_ARGS(thread, mask, info),
+	TP_PROTO(struct evl_thread *thread, int mask),
+	TP_ARGS(thread, mask),
 
 	TP_STRUCT__entry(
 		__string(name, thread->name)
 		__field(pid_t, pid)
 		__field(int, mask)
-		__field(int, info)
 	),
 
 	TP_fast_assign(
 		__assign_str(name);
 		__entry->pid = evl_get_inband_pid(thread);
 		__entry->mask = mask;
-		__entry->info = info;
 	),
 
-	TP_printk("name=%s pid=%d mask=%#x info=%#x",
-		__get_str(name), __entry->pid,
-		__entry->mask, __entry->info)
+	TP_printk("name=%s pid=%d mask=%#x",
+		__get_str(name), __entry->pid, __entry->mask)
 );
 
 TRACE_EVENT(evl_thread_fault,
