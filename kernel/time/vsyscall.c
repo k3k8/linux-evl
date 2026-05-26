dd0dbcc587d2c7dcea054713c2b964265d644c69
Florian Bezdeka <florian.bezdeka@siemens.com>
2026-05-26 12:31:57 +0200

    lib/vdso: Fix build failure around CONFIG_GENERIC_VDSO_CLOCKSOURCE
    During the forward porting to 6.18 the signature of
    update_generic_mmio() changed and introduced one undeclared variable.
    
    At the same time the name of the kconfig variable got invalid.
    
    Signed-off-by: Florian Bezdeka <florian.bezdeka@siemens.com>

--- kernel/time/vsyscall.c
+++ kernel/time/vsyscall.c
@@ -77,7 +77,8 @@ static inline void update_vdso_data(struct vdso_data *vdata,
 
 static void update_generic_mmio(struct vdso_data *vdata, struct clocksource *cs)
 {
-#ifdef CONFIG_GENERIC_CLOCKSOURCE_VDSO
+#ifdef CONFIG_GENERIC_VDSO_CLOCKSOURCE
+	struct vdso_clock *vc = vdata->clock_data;
 	u16 seq;
 
 	if (cs->vdso_type == (vdata->cs_type_seq >> 16))
