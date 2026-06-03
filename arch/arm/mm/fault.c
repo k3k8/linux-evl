f70cdc639b90521d925e8f3424d48dc412f7404c
Florian Bezdeka <florian.bezdeka@siemens.com>
2026-06-03 17:07:15 +0200

    arm: irq_pipeline: Fix fault_{entry,exit} imbalance
    upstream commit
    d92725256b4f ("mm: avoid unnecessary page fault retires on shared memory types")
    introduced a new exit path to do_page_fault(), so that an imbalance
    of fault_{entry,exit} can happen.
    
    The commit was introduced with Linux 6.0, so all newer Dovetail
    versions are affected.
    
    Signed-off-by: Florian Bezdeka <florian.bezdeka@siemens.com>

--- arch/arm/mm/fault.c
+++ arch/arm/mm/fault.c
@@ -458,7 +458,7 @@ retry:
 
 	/* The fault is fully completed (including releasing mmap lock) */
 	if (fault & VM_FAULT_COMPLETED)
-		return 0;
+		goto out;
 
 	if (!(fault & VM_FAULT_ERROR)) {
 		if (fault & VM_FAULT_RETRY) {
